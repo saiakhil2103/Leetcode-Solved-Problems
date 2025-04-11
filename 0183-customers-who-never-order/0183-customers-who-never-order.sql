8a86f474277775553438c5009173ff90da0550be
SAI AKHIL PODDUTURI <68911307+saiakhil2103@users.noreply.github.com>
2025-04-11 11:00:15 +0530

    Time: 593 ms (77.18%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,3 +1,6 @@
-with cte as 
-(SELECT name, Orders.customerId as cid FROM Customers LEFT JOIN Orders ON Customers.id = Orders.customerId)
-select name as Customers from cte where cid is null;
+-- with cte as 
+-- (SELECT name, Orders.customerId as cid FROM Customers LEFT JOIN Orders ON Customers.id = Orders.customerId)
+-- select name as Customers from cte where cid is null;
+
+
+select name as Customers from Customers where id not in (select customerId from Orders);
