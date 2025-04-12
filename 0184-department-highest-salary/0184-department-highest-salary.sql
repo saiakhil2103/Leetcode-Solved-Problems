6a0e518846305a0484f5a36d711ef2923d9bec09
SAI AKHIL PODDUTURI <68911307+saiakhil2103@users.noreply.github.com>
2025-04-12 15:29:20 +0530

    Time: 645 ms (89.89%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,7 +1,14 @@
-select d.name as Department, e.name as Employee, e.salary as Salary 
-from (
-    select *, rank() over (partition by departmentId order by salary desc) rnk from Employee
-) e 
-left join Department d 
-on e.departmentId = d.id
-where e.rnk = 1;
+-- select d.name as Department, e.name as Employee, e.salary as Salary 
+-- from (
+--     select *, rank() over (partition by departmentId order by salary desc) rnk from Employee
+-- ) e 
+-- left join Department d 
+-- on e.departmentId = d.id
+-- where e.rnk = 1;
+
+select dept.name as Department,
+    emp.name as Employee,
+    emp.salary as Salary
+from Department dept, Employee emp 
+where emp.departmentId = dept.id and (emp.departmentId, emp.salary) in 
+(select departmentId, max(salary) from Employee group by departmentId);
