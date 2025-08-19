8ee1e1580a5e5c1742ed381d3f8af93880b645b1
SAI AKHIL PODDUTURI <68911307+saiakhil2103@users.noreply.github.com>
2025-08-19 16:45:32 +0530

    Time: 58 ms (23.2%), Space: 28.4 MB (42.33%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        answer=0
+        consecutive_zeros=0
+        for index, value in enumerate(nums):
+            if value==0:
+                i=index-1
+                answer+=consecutive_zeros+1
+                consecutive_zeros+=1
+            else:
+                consecutive_zeros=0
+        return answer
+                    
