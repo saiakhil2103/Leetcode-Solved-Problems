ee2828e19074c438829a31cc7e262a44b1cb0ca0
SAI AKHIL PODDUTURI <68911307+saiakhil2103@users.noreply.github.com>
2025-09-02 11:08:07 +0530

    Time: 15 ms (12.79%), Space: 25.5 MB (5.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,12 @@
-import re 
+from collections import deque 
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
-        return cleaned == cleaned[::-1]
+        cleaned = "".join(ch.lower() for ch in s if ch.isalnum())
+        def helper(left, right):
+            if left >= right:
+                return True 
+            if cleaned[left] != cleaned[right]:
+                return False
+            return helper(left+1, right-1)
+        return helper(0, len(cleaned)-1)
