f30f3c024e00fcbd7c764bf15455f702eef566ca
SAI AKHIL PODDUTURI <68911307+saiakhil2103@users.noreply.github.com>
2025-09-03 14:45:27 +0530

    Time: 3367 ms (5.01%), Space: 151 MB (6.52%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,26 +1,25 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        res, quad = [], []
-        def k_sum(k, start, target):
-            if k != 2:
-                for i in range(start, len(nums) - k + 1):
-                    if i > start and nums[i] == nums[i-1]:
-                        continue
-                    quad.append(nums[i])
-                    k_sum(k-1, i+1, target-nums[i])
-                    quad.pop() 
-                return 
-            l, r = start, len(nums)-1
-            while l < r:
-                if nums[l] + nums[r] < target:
-                    l += 1 
-                elif nums[l] + nums[r] > target:
-                    r -= 1
-                else:
-                    res.append(quad + [nums[l], nums[r]])
-                    l += 1 
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1 
-        k_sum(4, 0, target)
-        return res            
+        result = []
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                for k in range(j+1, n):
+                    total_sum = nums[i]+nums[j]+nums[k]
+                    left, right = k+1, n-1
+                    while left<=right:
+                        mid=(left+right)//2
+                        if nums[mid] == target-total_sum:
+                            temp = [nums[i], nums[j], nums[k], target-total_sum] 
+                            result.append(temp)
+                            break
+                        elif nums[mid] > target-total_sum:
+                            right = mid - 1
+                        else:
+                            left = mid + 1
+        updated_result = []
+        for res in result:
+            if sorted(res) not in updated_result:
+                updated_result.append(sorted(res))
+        return updated_result
