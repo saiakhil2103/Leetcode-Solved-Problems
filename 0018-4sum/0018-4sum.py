2b43863c9754a1503ba51008e9ab003946fb759f
SAI AKHIL PODDUTURI <68911307+saiakhil2103@users.noreply.github.com>
2025-09-03 14:56:07 +0530

    Time: 542 ms (18.58%), Space: 17.9 MB (36.92%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -4,22 +4,25 @@ class Solution:
         result = []
         n = len(nums)
         for i in range(n):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             for j in range(i+1, n):
-                for k in range(j+1, n):
-                    total_sum = nums[i]+nums[j]+nums[k]
-                    left, right = k+1, n-1
-                    while left<=right:
-                        mid=(left+right)//2
-                        if nums[mid] == target-total_sum:
-                            temp = [nums[i], nums[j], nums[k], target-total_sum] 
-                            result.append(temp)
-                            break
-                        elif nums[mid] > target-total_sum:
-                            right = mid - 1
-                        else:
-                            left = mid + 1
-        updated_result = []
-        for res in result:
-            if sorted(res) not in updated_result:
-                updated_result.append(sorted(res))
-        return updated_result
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                left, right = j+1, n-1
+                while left<right:
+                    curr_sum = nums[i] + nums[j]
+                    rem_sum = target - curr_sum
+                    if nums[left]+nums[right] == rem_sum:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left<right and nums[left] == nums[left+1]:
+                            left+=1
+                        while left<right and nums[right] == nums[right-1]:
+                            right-=1
+                        left+=1
+                        right-=1
+                    elif nums[left]+nums[right] > rem_sum:
+                        right -= 1
+                    else:
+                        left += 1
+        return result
